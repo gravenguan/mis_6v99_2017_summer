c842842c74f742c03cbcb0d63843ce8d2054251e
gravenguan <graven@tamu.edu>
2017-06-16 18:51:43 -0600

    ...

--- lab_01/print_flight_schedule.py
+++ lab_01/print_flight_schedule.py
@@ -5,6 +5,23 @@ Created on Fri Jun 09 18:48:55 2017
 @author: syghr
 """
 
-airport=[['AUS',1,25],['DAL',2,30],['HOU',3,35]]
 
-flight_time=[['AUS','DAL',45]]
+
+csv_header = 'tail_number,origin,destination,departure_time,arrival_time'
+file_name = 'flight_schedule.csv'
+def print_flight_schedule(fn, csv_hdr, flt_sched):
+     with open(fn,'wt') as f:
+         print(csv_hdr, file=f)
+         for s in flt_sched:
+             print(','.join(s), file=f)
+
+# add the flights for T4, T5 and T6 to the list of lists below
+flight_schedule = [['T1','AUS','DAL','0600','0650'],
+                   ['T2','DAL','HOU','0600','0705'],
+                   ['T3','HOU','AUS','0600','0645'],
+                   ['T4','DAL','AUS','0600','0650'],
+                   ['T5','HOU','DAL','0600','0705'],
+                   ['T6','AUS','HOU','0600','0645']]
+
+
+print_flight_schedule(file_name, csv_header, flight_schedule)                  
